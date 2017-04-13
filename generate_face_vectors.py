8846889aaf93fea6f3d78dbde2ca548d49552e06
Willem Koopman <willem.koopman@ft.com>
2017-04-13 16:24:58 +0100

    we almost have an autodownloader

---
+++ generate_face_vectors.py
@@ -0,0 +1,37 @@
+import os
+import sys
+import json
+import requests
+
+try:
+    capiKey = os.environ['CAPI_KEY']
+    msKey   = os.environ['MS_KEY']
+except:
+    print "please set the env-var CAPI_KEY with a valid key"
+    sys.exit(3)
+r = requests.get('http://api.ft.com/sixdegrees/mostMentionedPeople?fromDate=2017-03-07&limit=100&apiKey={0}'.format(capiKey))
+if r.status_code == requests.codes.ok:
+    names = r.json()
+else:
+    print "got a {0} error trying to talk to capi".format(r.status_code)
+    
+
+print names[0]
+for name in names:
+    idUrl = name['id']
+    name = name['prefLabel']
+    nameParts = name.split(' ')
+    shortName = u"{0} {1}".format(nameParts[0], nameParts[-1])
+    #print idUrl+'&apiKey={0}'.format(capiKey)
+    print shortName
+    search = requests.get('https://api.cognitive.microsoft.com/bing/v5.0/images/search?q={0}&count=5&mrkt=en-GB'.format(shortName), headers= {'Ocp-Apim-Subscription-Key': msKey})
+    print search.status_code
+    for count, image in enumerate(search.json()['value']):
+        imageReq = requests.get(image['contentUrl'], stream=True)
+        with open("candidates/{0}-{1}.jpg".format(shortName.replace(' ', '-'),count), 'wb') as imageFile:
+            for chunk in imageReq.iter_content(chunk_size=1024):
+                imageFile.write(chunk)
+
+    break
+
+
